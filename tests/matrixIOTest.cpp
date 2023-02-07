98b79ee335498d1fb64590cb1d67e1930d15bf42
Maximilian Kuhn <maximilian.kuhn@ymail.com>
2023-02-07 18:55:42 +0100

    Fix

--- tests/matrixIOTest.cpp
+++ tests/matrixIOTest.cpp
@@ -1,20 +1,18 @@
 #define BOOST_TEST_DYN_LINK
+#include "matrixIO.hpp"
 #include <Eigen/Dense>
 #include <boost/test/unit_test.hpp>
-#include "matrixIO.hpp"
 
 using namespace Eigen;
 
 BOOST_AUTO_TEST_SUITE(matrixIOTests, *boost::unit_test::tolerance(1e-12))
 
-BOOST_AUTO_TEST_CASE(openData)
-{
+BOOST_AUTO_TEST_CASE(openData) {
   MatrixXd matrix(3, 3);
   matrix << matrixIO::openData("../data/m3.csv", 3);
 
   MatrixXd expectedMatrix(3, 3);
-  expectedMatrix << 0.680375, 0.59688, -0.329554,
-      -0.211234, 0.823295, 0.536459,
+  expectedMatrix << 0.680375, 0.59688, -0.329554, -0.211234, 0.823295, 0.536459,
       0.566198, -0.604897, -0.444451;
 
   BOOST_TEST(matrix(0, 0) == expectedMatrix(0, 0));
